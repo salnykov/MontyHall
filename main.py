e406c8f69569ae9aa94e8999a190a2b9bc499682
mykhaylosalnykov <salnykov@gmail.com>
2023-05-03 14:55:20 +0200

    Monty Hall Commit

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import random
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def doors():
+    result = ['goat', 'goat', 'goat']
+    result[random.randint(0, 2)]='car'
+    return result
+
+def choice():
+    return random.randint(0, 2)
+
+def nochange():
+    if doors()[choice()] == 'car':
+        return 'win'
+    else:
+        return 'loose'
+
+def change():
+    if doors()[choice()] == 'goat':
+        return 'win'
+    else:
+        return 'loose'
+
+def simulation (iterations, strategy):
+    wincount = 0
+    for _ in range(iterations-1):
+        if strategy() == 'win':
+            wincount += 1
+    return wincount/iterations
+
+change_outcomes = []
+nochange_outcomes = []
+
+for _ in range(5000):
+    change_outcomes.append(simulation(100, change))
+    nochange_outcomes.append(simulation(100, nochange))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+plt.subplot(1, 2, 2)
+plt.hist(change_outcomes, bins=10)
+plt.title("Win chance if CHANGE")
+plt.xlabel('Probability')
+plt.ylabel('Frequency')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+plt.subplot(1, 2, 1)
+plt.hist(nochange_outcomes, bins=10)
+plt.title("Win chance if NO CHANGE")
+plt.xlabel('Probability')
+plt.ylabel('Frequency')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
